f5a7acf9ade5f21863515abe6349ceab21356d75
JellyToon <63642776+JellyToon@users.noreply.github.com>
2021-05-30 19:55:41 +0900

    Camera
    카메라 벽 뚫기 방지, TPS 구현
    Map 수정

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,18 +4,83 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
-    
+    [SerializeField] Transform target;
+    [SerializeField] Transform cameraRig;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float dist = 5f;
+
+    [SerializeField] float xSpeed = 90f;
+    [SerializeField] float ySpeed = 70f;
+
+    [SerializeField] float yMinLimit = -1f;
+    [SerializeField] float yMaxLimit = 45f;
+
+    private float x;
+    private float y;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Vector3 angles = transform.eulerAngles;
+
+        x = target.rotation.x;
+        y = target.rotation.y;
+
+        //x = angles.x;
+        //y = angles.y;
     }
 
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position = target.position + offset;
+        if(target)
+        {
+            if(dist >=5f)
+            {
+                dist = 5f;
+            }
+
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            Vector3 position = rotation * new Vector3(0f, 0f, -dist) + target.position + new Vector3(0f, 0f, 0f);
+            position.y += 1.5f;
+
+            transform.rotation = rotation;
+            transform.position = position;
+
+        }
+
+        RaycastHit hit;
+        Vector3 rayDir = (transform.position - cameraRig.position).normalized;
+        LayerMask layerMask = 1 << LayerMask.NameToLayer("Player");
+        if (Physics.Raycast(cameraRig.position, rayDir, out hit, 5f, ~layerMask))
+        {
+            dist = Vector3.Distance(cameraRig.position, hit.point) - 2f;
+        }
+        else
+            dist = 5f;
+
+    }
+
+    float ClampAngle(float anlge, float min, float max)
+    {
+        if (anlge < -360f)
+        {
+            anlge += 360f;
+        }
+        if (anlge > 360f)
+        {
+            anlge -= 360f;
+        }
+
+        return Mathf.Clamp(anlge, min, max);
+
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,34 +13,70 @@ public class Player : MonoBehaviour
 	Vector3 moveVec;
 
 	Animator anim;
+	[SerializeField] Transform cameraTransform;
 
 	// Start is called before the first frame update
-    void Awake()
+	void Awake()
     {
 		anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+	{
+		//GetInput();
+		Move();
+		//moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+		//if(rDown)
+  //      {
+		//	transform.position += moveVec * speed * 1.5f * Time.deltaTime;
+		//}
+  //      else
+  //      {
+		//	transform.position += moveVec * speed * Time.deltaTime;
+		//}
+
+		//anim.SetBool("isWalk", moveVec != Vector3.zero);
+		//anim.SetBool("isRun", rDown);
+
+		//transform.LookAt(transform.position + moveVec);
+	}
+	void GetInput()
+	{
+	}
+
+	void Move()
+	{
 		hAxis = Input.GetAxisRaw("Horizontal");
 		vAxis = Input.GetAxisRaw("Vertical");
 		rDown = Input.GetButton("Run");
 
-		moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+		Vector3 moveInput = new Vector3(hAxis, 0f, vAxis);
 
-		if(rDown)
+        //bool isMove = moveInput.magnitude != 0;
+        //anim.SetBool("isRun", isMove);
+
+        //if (isMove)
+        //{
+        Vector3 lookForward = new Vector3(cameraTransform.forward.x, 0f, cameraTransform.forward.z).normalized;
+        Vector3 lookRight = new Vector3(cameraTransform.right.x, 0f, cameraTransform.right.z).normalized;
+        Vector3 moveDir = lookForward * moveInput.z + lookRight * moveInput.x;
+        moveDir.Normalize();
+
+        transform.forward = moveDir;
+        if (rDown)
         {
-			transform.position += moveVec * speed * 1.5f * Time.deltaTime;
-		}
+            transform.position += moveDir * speed * 1.5f * Time.deltaTime;
+        }
         else
         {
-			transform.position += moveVec * speed * Time.deltaTime;
-		}
+            transform.position += moveDir * speed * Time.deltaTime;
+        }
 
-		anim.SetBool("isWalk", moveVec != Vector3.zero);
-		anim.SetBool("isRun", rDown);
 
-		transform.LookAt(transform.position + moveVec);
+        anim.SetBool("isWalk", moveDir != Vector3.zero);
+        anim.SetBool("isRun", rDown);
+		//}
 	}
 }
